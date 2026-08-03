2f9589c0506cfb6f43aed5fb201ba641357e5ad6
Sathiyagandhi Karunanidhi <sathiyagandhi.k@syncfusion.com>
2026-08-03 15:51:58 +0530

    Added the sample folder

---
+++ MaskedEntrySample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MaskedEntrySample
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+       
+
+       
+    }
+    
+}
